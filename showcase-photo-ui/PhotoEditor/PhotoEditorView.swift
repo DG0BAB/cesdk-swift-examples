fd4e2808b3dd2f728a28c6ebb1ac323b6463cf71
Joachim Deelen <jdeelen@micabo.de>
2022-12-15 14:42:39 +0100

    This will be the future main view for the photo editor

--- showcase-photo-ui/PhotoEditor/PhotoEditorView.swift
+++ showcase-photo-ui/PhotoEditor/PhotoEditorView.swift
@@ -5,4 +5,37 @@
 //  Created by Joachim Deelen on 15.12.22.
 //
 
-import Foundation
+import SwiftUI
+
+/// Presents a list of `Photo`s from which one can be selected by the user
+struct PhotoEditorView: View {
+
+  @Environment(\.dismiss) private var dismiss
+
+  private var photo: PhotosManager.Photo
+
+
+  /// Initializes this `View` with the `Photo`s to be edited
+  ///
+  /// - Parameters:
+  ///   - photo: The `Photo` to be edited
+  init(photo: PhotosManager.Photo) {
+    self.photo = photo
+  }
+
+  public var body: some View {
+    Text(photo.id)
+      .background {
+        Color.green
+      }
+      .onTapGesture {
+        dismiss()
+      }
+  }
+}
+
+struct PhotoEditor_Previews: PreviewProvider {
+  static var previews: some View {
+    PhotoEditorView(photo: PhotosManager.default.photos[1])
+  }
+}
