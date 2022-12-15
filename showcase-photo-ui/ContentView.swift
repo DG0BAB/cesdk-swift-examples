72295b032fa0ea25f46a297b4d64dd5289e0d90d
Joachim Deelen <jdeelen@micabo.de>
2022-12-15 14:40:16 +0100

    First (basic) implementation for the main view of the photo-ui showcase.

--- showcase-photo-ui/ContentView.swift
+++ showcase-photo-ui/ContentView.swift
@@ -1,52 +1,28 @@
 import SwiftUI
 
-private class BundleIdentifier {}
+/// Starting point for the photo-ui showcase
+///
+/// Presents a list of `Pphoto`s available for editing.
+/// On selection of a `Photo` the `PhotoEditor` gets presented.
+/// When editing ends, the list of available `Photo`s is presented again
+public struct ContentView: View {
 
-public struct ContentsView: View {
-//  @StateObject private var interactor = Interactor()
-
-  @Environment(\.layoutDirection) private var layoutDirection
-
-  private let zoomPadding: CGFloat = 16
-  private let isEditing: Bool = false
-  private let bundle: Bundle = Bundle.init(for: BundleIdentifier.self)
-  @State private var interactivePopGestureRecognizer: UIGestureRecognizer?
+  @State private var orientation = UIDevice.current.orientation
+  @State private var selection: PhotosManager.Photo? = nil
 
   public init() {}
   
   public var body: some View {
-    List {
-      Image("woman", bundle: bundle)
-    }
-//    Canvas(zoomPadding: zoomPadding)
-//      .allowsHitTesting(isEditing)
-//      .navigationBarTitleDisplayMode(.inline)
-//      .navigationBarBackButtonHidden(!isEditing)
-//      .introspectNavigationController { navigationController in
-//        // Disable swipe-back gesture and restore `onDisappear`
-//        interactivePopGestureRecognizer = navigationController.interactivePopGestureRecognizer
-//        interactivePopGestureRecognizer?.isEnabled = false
-//      }
-//      .toolbarBackground(.visible, for: .navigationBar)
-//      .toolbar {
-//        ToolbarItemGroup(placement: .principal) {
-//        }
-//        ToolbarItem(placement: .navigationBarTrailing) {
-//        }
-//      }
-//      .onAppear {
-//      }
-//      .onWillDisappear {
-//      }
-//      .onDisappear {
-//        interactivePopGestureRecognizer?.isEnabled = true
-//      }
-
+    PhotoSelectionView(photos: PhotosManager.default.photos, selection: $selection)
+      .fullScreenCover(item: $selection) { photo in
+        PhotoEditorView(photo: photo)
+          .onDisappear { selection = nil }
+      }
   }
 }
 
 struct ContentView_Previews: PreviewProvider {
   static var previews: some View {
-    ContentsView()
+    ContentView()
   }
 }
