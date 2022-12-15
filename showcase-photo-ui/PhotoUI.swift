019b38ae0383634e3fe46230d830ca8966b2057b
Joachim Deelen <jdeelen@micabo.de>
2022-12-15 14:40:39 +0100

    First implementation of the PhotoUI principal class

--- showcase-photo-ui/PhotoUI.swift
+++ showcase-photo-ui/PhotoUI.swift
@@ -6,3 +6,10 @@
 //
 
 import Foundation
+
+/// Principal class for the photo-ui showcase framework
+final class PhotoUI {
+
+  /// The photo-ui showcase framework `Bundle`
+  static let bundle: Bundle = Bundle.init(for: PhotoUI.self)
+}
